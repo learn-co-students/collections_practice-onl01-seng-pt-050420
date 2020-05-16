9c7dd32bb65f226f585e043da5e0e28f448dcee7
Jack Quinoa <jquinoa208@gmail.com>
2020-05-16 04:47:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,61 @@
+def sort_array_asc(integers)
+  integers.sort
+end
+
+def sort_array_desc(integers)
+  integers.sort do |a , b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a , b|
+    a.length <=> b.length 
+  end
+end
+
+def swap_elements(elements)
+  elements[0], elements[1], elements[2] = elements[0], elements[2], elements[1]
+end
+
+def reverse_array(integers)
+ integers.reverse 
+end
+
+def kesha_maker(strings)
+  strings.each do |string|
+    string[2] = "$"
+  end
+end
+
+def find_a(strings)
+  strings.select do |string|
+    string.start_with?("a") 
+  end
+end 
+
+def sum_array(integers)
+  integers.inject do |sum, n|
+  sum + n 
+  end
+end
+
+def add_s(words)
+  words.map do |word|  
+    if word == words[1] 
+      word
+    else
+       word + "s"
+    end
+  end
+end
+
+  
+
+  
+  
+  
+  
+  
+  
 
